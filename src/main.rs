8fdc75680373ae61314650146c13b174b579da05
saatvik-10 <saatvikmadan529@gmail.com>
2025-09-28 02:34:29 +0530

    added transaction history

--- src/main.rs
+++ src/main.rs
@@ -8,35 +8,37 @@ fn main() {
     let rpc_url = String::from("https://api.devnet.solana.com");
     let client = RpcClient::new_with_commitment(rpc_url, CommitmentConfig::confirmed());
 
-    // println!("Connected to  Devnet...");
+    println!("\nBasic devnet connection...\n");
+    println!("Connected to  Devnet...");
 
-    // match client.get_slot() {
-    //     Ok(slot) => println!("Current slot is: {}", slot),
-    //     Err(e) => println!("Error getting slot: {}", e),
-    // }
+    match client.get_slot() {
+        Ok(slot) => println!("Current slot is: {}", slot),
+        Err(e) => println!("Error getting slot: {}", e),
+    }
 
     let address_sol = "So11111111111111111111111111111111111111112";
 
-    // match Pubkey::from_str(address_sol) {
-    //     Ok(pubkey) => {
-    //         println!("Checking address: {}", address_sol);
+    println!("\nQuerying Transaction...\n");
+    match Pubkey::from_str(address_sol) {
+        Ok(pubkey) => {
+            println!("Checking address: {}", address_sol);
 
-    //         match client.get_balance(&pubkey) {
-    //             Ok(balance) => {
-    //                 let balance_sol = balance as f64 / 1000_000_000.0;
-    //                 println!("Balance: {} lamports ({:.9} SOL)", balance, balance_sol)
-    //             }
-    //             Err(e) => println!("Error fetching balance: {}", e),
-    //         }
-    //     }
-    //     Err(e) => println!("Invalid Address: {}", e),
-    // }
+            match client.get_balance(&pubkey) {
+                Ok(balance) => {
+                    let balance_sol = balance as f64 / 1000_000_000.0;
+                    println!("Balance: {} lamports ({:.9} SOL)", balance, balance_sol)
+                }
+                Err(e) => println!("Error fetching balance: {}", e),
+            }
+        }
+        Err(e) => println!("Invalid Address: {}", e),
+    }
 
+    println!("\nRecent Trasaction Sigs...\n");
     match Pubkey::from_str(address_sol) {
         Ok(pubkey) => {
             println!("Fetching transaction history for: {}", address_sol);
 
-            //recent trasaction sigs
             match client.get_signatures_for_address(&pubkey) {
                 Ok(sigs) => {
                     let limited_sigs = sigs.iter().take(5);
